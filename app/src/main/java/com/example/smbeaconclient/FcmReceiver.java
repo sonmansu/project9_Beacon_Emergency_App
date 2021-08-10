b4e211a78199c8afc2dfd702599c55d0b2b206b0
sonmansu <gjdmstnals@pusan.ac.kr>
2021-08-11 01:32:10 +0900

    Added Broadcast receiver
    - modify FCM onMessageReceived
      - read FCM data payload
      - send broadcast
    - add broadcast receiver
    
    - doesn't work in foreground

---
+++ app/src/main/java/com/example/smbeaconclient/FcmReceiver.java
@@ -0,0 +1,29 @@
+package com.example.smbeaconclient;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class FcmReceiver extends BroadcastReceiver {
+    String TAG = "FcmReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("example.test.broadcast")) {
+            Toast.makeText(context, "Customize broadcast!", Toast.LENGTH_LONG).show();
+        }
+        Log.d(TAG, "sendbroadcastonReceive");
+        Intent i = new Intent(context, EmergencyActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+//        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //는 안되더라
+//        i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP |Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+
+
+        context.startActivity(i);
+
+    }
+}
--- app/src/main/java/com/example/smbeaconclient/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/smbeaconclient/firebase/MyFirebaseMessagingService.java
@@ -1,14 +1,28 @@
 package com.example.smbeaconclient.firebase;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.graphics.drawable.IconCompat;
 
 import com.example.smbeaconclient.EmergencyActivity;
+import com.example.smbeaconclient.FcmReceiver;
+import com.example.smbeaconclient.MainActivity;
+import com.example.smbeaconclient.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -19,46 +33,136 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static String TAG = "MyFirebaseMessagingService";
-
-    public void showDataMessage(String msgTitle, String msgContent) {
-        Log.i("### data msgTitle : ", msgTitle);
-        Log.i("### data msgContent : ", msgContent);
-        String toastText = String.format("[Data 메시지] title: %s => content: %s", msgTitle, msgContent);
-        Looper.prepare();
-        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG).show();
-        Looper.loop();
-    }
-
-    /**
-     * 수신받은 메시지를 Toast로 보여줌
-     * @param msgTitle
-     * @param msgContent
-     */
-    public void showNotificationMessage(String msgTitle, String msgContent) {
-        Log.i("### noti msgTitle : ", msgTitle);
-        Log.i("### noti msgContent : ", msgContent);
-        String toastText = String.format("[Notification 메시지] title: %s => content: %s", msgTitle, msgContent);
-        Looper.prepare();
-        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG).show();
-        Looper.loop();
-    }
+    Bitmap bitmap;
     @Override
-    public void onMessageReceived(RemoteMessage msg) {
-        Log.d(TAG, "onMessageReceived called");
-        Log.i("### msg : ", msg.toString());
-        if (msg.getData().isEmpty()) {
-            showNotificationMessage(msg.getNotification().getTitle(), msg.getNotification().getBody());  // Notification으로 받을 때
-        } else {
-            showDataMessage(msg.getData().get("title"), msg.getData().get("content"));  // Data로 받을 때
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        //
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
         }
 
-        Intent i = new Intent(getApplicationContext(), EmergencyActivity.class);
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        //The message which i send will have keys named [message, image, AnotherActivity] and corresponding values.
+        //You can change as per the requirement.
+
+        //message will contain the Push Message
+        String message = remoteMessage.getData().get("message");
+        //imageUri will contain URL of the image to be displayed with Notification
+        String imageUri = remoteMessage.getData().get("image");
+        Log.d(TAG, "imageUri: " + imageUri);
+
+        //If the key AnotherActivity has  value as True then when the user taps on notification, in the app AnotherActivity will be opened.
+        //If the key AnotherActivity has  value as False then when the user taps on notification, in the app MainActivity will be opened.
+        String TrueOrFlase = remoteMessage.getData().get("AnotherActivity");
+
+        //To get a Bitmap image from the URL received
+        bitmap = getBitmapfromUrl(imageUri);
+
+        final Intent intent = new Intent(getApplicationContext(), FcmReceiver.class); // Receiver 설정
+        intent.setAction("example.test.broadcast");
+        sendBroadcast(intent);
+        Log.d(TAG, "SendBroadcast");
+
+        sendNotification(message, bitmap, TrueOrFlase);
+
+
+
+        //앱이실행중일때만 동작함
+//        Intent intent = new Intent();
+//        intent.setAction(Intent.ACTION_MAIN);
+//        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        ComponentName cn = new ComponentName(this, EmergencyActivity.class);
+//        intent.setComponent(cn);
+//        startActivity(intent);
+
+
+        //
+//        final PendingIntent pendingIntent = PendingIntent.getBroadcast(
+//                getApplicationContext(), 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//
+//        final AlarmManager manager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+//        /* 설정된 시각에 알람 설정
+//        인자 : 알람매니저 타입 / 언제 이 알람이 발동되야하는지 그 시간을 long값으로 줘야함 / 뭐가 실행될지는 펜딩인덴트로 동작하게 함. */
+//        manager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, , pendingIntent); //setExact()과 동일하만 절전모드에서도 동작하는 함수
+////        manager.setAlarmClock(new AlarmManager.AlarmClockInfo(alarm.getTIME(), pendingIntent), pendingIntent);
+//
+//        TimeUtils.printTimeStringDebug("알람서비스등록", alarm.getTIME(), alarm.getRequestCode());
+
+
+
     }
 
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     */
+
+    private void sendNotification(String messageBody, Bitmap image, String TrueOrFalse) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("AnotherActivity", TrueOrFalse);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setLargeIcon(image)/*Notification icon image*/
+//                .setSmallIcon(R.drawable.app_icon)
+                .setSmallIcon(IconCompat.createWithBitmap(image))
+                .setContentTitle(messageBody)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(image))/*Notification with Image*/
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    /*
+     *To get a Bitmap image from the URL received
+     * */
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
+    }
     /**
      * There are two scenarios when onNewToken is called:
      * 1) When a new token is generated on initial app startup
