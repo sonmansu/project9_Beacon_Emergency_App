106cdbbed76c72dc75d6e1f1db553752c499c135
woo-l-s <tjrdn110@naver.com>
2021-08-16 18:04:55 +0900

    improve emergency

--- app/src/main/java/com/example/smbeaconclient/EmergencyActivity.java
+++ app/src/main/java/com/example/smbeaconclient/EmergencyActivity.java
@@ -10,5 +10,6 @@ public class EmergencyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+
     }
 }
